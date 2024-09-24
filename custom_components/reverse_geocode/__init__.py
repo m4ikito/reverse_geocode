bc28f4194e644899c5315e8b9cf4896e537db926
m4ikito <61995075+m4ikito@users.noreply.github.com>
2024-09-24 12:31:54 +0200

    __init__.py aktualisieren

--- custom_components/reverse_geocode/__init__.py
+++ custom_components/reverse_geocode/__init__.py
@@ -1,1 +1,19 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Setze die Integration mit der Konfiguration aus dem Konfigurationsfluss auf."""
+    hass.data.setdefault(DOMAIN, {})
+
+    # Hier könntest du das Setup deiner Integration auf Basis der entry-Daten vornehmen
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Entferne eine Instanz der Integration."""
+    if entry.entry_id in hass.data[DOMAIN]:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
