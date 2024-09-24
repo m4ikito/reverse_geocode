27c319df5ea508cd905eb92a0138407189fc01f3
m4ikito <61995075+m4ikito@users.noreply.github.com>
2024-09-24 14:30:55 +0200

    config_flow.py aktualisieren

--- custom_components/reverse_geocode/config_flow.py
+++ custom_components/reverse_geocode/config_flow.py
@@ -1,23 +1,20 @@
-import logging
 from homeassistant import config_entries
 import voluptuous as vol
-from homeassistant.const import CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_NAME
 
-_LOGGER = logging.getLogger(__name__)
+DOMAIN = "reverse_geocode"
 
-class ReverseGeocodeConfigFlow(config_entries.ConfigFlow, domain='reverse_geocode'):
+class ReverseGeocodeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is None:
-            return await self.async_show_form(step_id="user")
+            return self.async_show_form(step_id="user", data_schema=vol.Schema({
+                vol.Required(CONF_NAME): str,  # Frage nach dem Namen
+            }))
+
+        # Überprüfen, ob der Benutzername eingegeben wurde
+        if CONF_NAME not in user_input:
+            return self.async_show_form(step_id="user", data_schema=vol.Schema({
+                vol.Required(CONF_NAME): str,
+            }))
 
         return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
-
-    async def async_show_form(self, step_id):
-        return self.async_show_form(
-            step_id=step_id,
-            data_schema=vol.Schema({
-                vol.Required(CONF_NAME): str,
-                vol.Required("device_tracker"): str,  # Name des device_tracker
-                vol.Required(CONF_SCAN_INTERVAL, default=60): int,  # Default auf 60 Sekunden
-            }),
-        )
