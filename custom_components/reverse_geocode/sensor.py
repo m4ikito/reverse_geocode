3d11e7ce4a01b758b2975b624134ae6be778a7cb
m4ikito <61995075+m4ikito@users.noreply.github.com>
2024-09-24 14:31:21 +0200

    sensor.py aktualisieren

--- custom_components/reverse_geocode/sensor.py
+++ custom_components/reverse_geocode/sensor.py
@@ -1,28 +1,33 @@
 from homeassistant.helpers.entity import Entity
-from homeassistant.const import CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.helpers import config_validation as cv
+from homeassistant.const import CONF_ENTITY_ID
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    sensor_name = config[CONF_NAME]
-    device_tracker = config["device_tracker"]
-    update_interval = config.get(CONF_SCAN_INTERVAL, 60)
+    device_tracker_entity_id = config.get(CONF_ENTITY_ID)
 
-    async_add_entities([ReverseGeocodeSensor(sensor_name, device_tracker, update_interval)])
+    # Überprüfen, ob der device_tracker vorhanden ist
+    device_tracker = hass.states.get(device_tracker_entity_id)
+    if device_tracker is None:
+        _LOGGER.error(f"Device tracker '{device_tracker_entity_id}' not found.")
+        return
+
+    # Werte abrufen
+    latitude = device_tracker.attributes.get("latitude")
+    longitude = device_tracker.attributes.get("longitude")
+
+    # Hier fügen wir Sensoren hinzu
+    async_add_entities([ReverseGeocodeSensor(latitude, longitude)], True)
 
 class ReverseGeocodeSensor(Entity):
-    def __init__(self, name, device_tracker, update_interval):
-        self._name = name
-        self._device_tracker = device_tracker
-        self._update_interval = update_interval
-        self._state = None
-
-    @property
-    def name(self):
-        return self._name
+    def __init__(self, latitude, longitude):
+        self._latitude = latitude
+        self._longitude = longitude
+        self._attr_name = "Reverse Geocode Sensor"  # Beispielname
 
     @property
     def state(self):
-        return self._state
+        return f"{self._latitude}, {self._longitude}"
 
-    async def async_update(self):
-        # Update-Logik für den Sensor hier implementieren
-        pass
+    @property
+    def name(self):
+        return self._attr_name
